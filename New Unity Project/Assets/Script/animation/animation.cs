db795caab5a67b8c91bd6d7c56bf33bf2ce094cc
silentshad <julien.boulicaut@epita.fr>
2016-06-14 11:49:42 +0200

    anim starting

---
+++ New Unity Project/Assets/Script/animation/animation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class animation : MonoBehaviour {
+
+    public Animator anim;
+    private float inputH;
+    private float inputV;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+        anim.SetFloat("inputH", inputH);
+        anim.SetFloat("inputV", inputV);
+
+    }
+}
