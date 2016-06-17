4fa792377e609a60dca37caf968898957c948262
daemondragon <bymerej78@epita.fr>
2016-06-17 10:33:58 +0200

    screenshot (F5) (not added on player)

---
+++ New Unity Project/Assets/Script/Misc/ScreenShot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class ScreenShot : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        enabled = hasAuthority;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.F5))
+        {
+            string filename = "Issidi:" + System.DateTime.Now.ToString();
+            Application.CaptureScreenshot(filename);
+        }
+	}
+}
